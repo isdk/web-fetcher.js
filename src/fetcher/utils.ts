f33bd6c1dd77e389f85c4a144753020e423891aa
Riceball LEE <snowyu.lee@gmail.com>
2025-10-20 17:10:26 +0800

    feat: add fetcher/utils.ts

---
+++ src/fetcher/utils.ts
@@ -0,0 +1,8 @@
+import { customAlphabet } from 'nanoid';
+
+// ============ ID 生成 ============
+const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 12);
+
+export function generateId(): string {
+  return nanoid();
+}
