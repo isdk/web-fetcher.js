3a1debd8c8f7d7cea0e8807541f4094f2b8f1e15
Riceball LEE <snowyu.lee@gmail.com>
2025-10-22 09:48:54 +0800

    feat: add WebFetcher

---
+++ src/fetcher/web-fetcher.ts
@@ -0,0 +1,32 @@
+import { FetchActionResultStatus } from "../action";
+import { FetchSession } from "./session";
+import { FetcherOptions, FetchResponse } from "./types";
+
+export class WebFetcher {
+  constructor(private defaults: FetcherOptions = {}) {}
+
+  async createSession(options?: FetcherOptions): Promise<FetchSession> {
+    const merged: FetcherOptions = { ...this.defaults, ...(options || {}) };
+    return new FetchSession(merged);
+  }
+
+  async fetch(options: FetcherOptions): Promise<FetchResponse|undefined>
+  async fetch(url: string|FetcherOptions, options?: FetcherOptions): Promise<FetchResponse|undefined> {
+    if (typeof url !== 'string') {
+      options = url;
+      url = options.url!;
+    }
+    const session = await this.createSession(options);
+    try {
+      const actions = options?.actions || [];
+      if (url && actions.findIndex(a => a.id === 'goto' && a.params?.url === url) !== 0) {
+        actions.unshift({ id: 'goto', params: { url } });
+      }
+      const response = await session.executeAll(actions);
+      // const response = await session.execute<'response'>({ id: 'goto', params: { url } });
+      return response;
+    } finally {
+      await session.dispose();
+    }
+  }
+}
