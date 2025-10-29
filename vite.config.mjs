5ffae3dd9ffea6aea31f046ee8dc13ffe4b1f6e9
Riceball LEE <snowyu.lee@gmail.com>
2025-10-29 18:20:21 +0800

    build: add test dir to test

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ['src/**/*.spec.ts', 'src/**/*.test.ts'],
+    include: ['src/**/*.spec.ts', 'src/**/*.test.ts', 'test/*.spec.ts', 'test/*.test.ts'],
     testTimeout: 80000,
     globals: true,
     "setupFiles": [
