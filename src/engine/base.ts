2d6a10dd2adcb361f582b5ab263f7f64b7f31024
Riceball LEE <snowyu.lee@gmail.com>
2026-07-29 21:18:25 +0800

    chore: remove unused vars

--- src/engine/base.ts
+++ src/engine/base.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import { defaultsDeep, merge } from 'lodash-es'
 import { EventEmitter } from 'events-ex'
-import { CommonError, ErrorCode } from '@isdk/common-error'
+import { ErrorCode } from '@isdk/common-error'
 import {
   Configuration,
   KeyValueStore,
